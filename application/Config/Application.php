3262a36f508dba2e6e77d4c05b49194b35597be3
Zsolt Sándor <ezsolt@gmail.com>
2016-04-28 23:49:25 +0200

    working with routes

--- application/Config/Application.php
+++ application/Config/Application.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Project config
+ * Application config
  *
  * @category  Config
  * @package   aaa
@@ -18,7 +18,6 @@ use Aura\Di\ContainerConfig;
 use Relay\Middleware\ExceptionHandler;
 use Relay\Middleware\ResponseSender;
 use Zend\Diactoros\Response as Response;
-use Portfolio\Domain\Entity\Post as PostEntity;
 
 // use Whoops\Run;
 
@@ -31,6 +30,8 @@ use Portfolio\Domain\Entity\Post as PostEntity;
  */
 class Application extends ContainerConfig
 {
+    private $views;
+    
     /**
      *
      * Defines params, setters, values, etc. in the Container.
@@ -51,8 +52,15 @@ class Application extends ContainerConfig
         $viewsconfig = new \Application\Config\AuraViews;
         $views = $viewsconfig->getViews();
 
+        $di->params['Radar\Adr\Handler\RoutingHandler']['matcher'] = $di->lazyGetCall('radar/adr:router', 'getMatcher');
+
+        // static page responder
+        $di->params['Application\Responder\AuraViewStaticPage']['views'] = $views;
+
+
+
+        //
         $di->params['Application\Responder\AuraViewResponder']['views'] = $views;
-        $di->params['Application\Responder\AuraViewStatic']['views'] = $views;
         $di->params['Application\Responder\AuraViewPayloadResponder']['views'] = $views;
     }
 
@@ -74,10 +82,10 @@ class Application extends ContainerConfig
         $adr->middle(new ExceptionHandler(new Response()));
         $adr->middle('Radar\Adr\Handler\RoutingHandler');
         $adr->middle('Radar\Adr\Handler\ActionHandler');
-		//
 
-		// $adr->middle(new Whoops\Run());
-		// $container->register(new \WhoopsPimple\WhoopsServiceProvider);
+
+
+        //$container->register(new \WhoopsPimple\WhoopsServiceProvider);
 
 
         /**
@@ -89,7 +97,7 @@ class Application extends ContainerConfig
         /**
          * Responder
          */
-        $adr->responder('Application\Responder\AuraViewResponder');
+        // $adr->responder('Application\Responder\AuraViewResponder');
 
         /**
          * Routes
@@ -111,31 +119,65 @@ class Application extends ContainerConfig
 
         // app routes
 
-        // static page views route
+        /**
+         *
+         * Static page views route
+         *
+         * @param input:
+         * @param responder:
+         * @param defaults: the default view to view when not defined
+         * @return tokens: the allowed values
+         *
+         */
         $adr->get('staticpage', '/page/{page}?', \Application\Domain\HelloPayload::class)
-            // ->input('Application\Input\MergedArray')
-            ->responder('Application\Responder\AuraViewStatic')
-            ->defaults(['page' => 'mikka']);
+            ->input('Application\Input\MergedArray')
+            ->responder('Application\Responder\AuraViewStaticPage')
+            ->defaults(['page' => 'mikka'])
+            ->tokens([
+                    'page' => '|mikka|mikka2|mikka3'
+                ]);
+
+
 
 
 
         // $adr->get('site.index', '/class/{name}?', ['Application\Domain\Index', '_invoke'])
         //     ->defaults(['name' => 'mikkamakka']);
 
-		// the responder here uses an array from the action
+        // the responder here uses an array from the action
         $adr->get('index', '/{name}?', \Application\Domain\Hello::class)
             // ->input('Application\Input\NoneExpected')
             // ->responder('Portfolio\Delivery\Responder\AuraViewResponder')
             ->defaults(['name' => 'world']);
 
-		// the responder here uses aura.payload object from the action
+        // the responder here uses aura.payload object from the action
         $adr->get('index2', '/payload/{page}?', \Application\Domain\HelloPayload::class)
             // ->input('Application\Input\NoneExpected')
             ->responder('Application\Responder\AuraViewPayloadResponder')
-            ->defaults(['page' => 'mikka']);
+            ->defaults(['page' => 'mikka'])
+            ->tokens([
+                    'page' => '|mikka|mikka2|mikka3'
+                ]);
 
     }
 
+
+    public function modifyRouter(Container $di)
+        {
+            $router = $di->get('radar/web-kernel:router');
+            
+            $router->add('aura.blog.browse', '/blog{/page}')
+                        ->setValues(array(
+                            'action' => 'aura.blog.browse',
+                            'page' => 1
+                        ))
+                        ->addTokens(array(
+                            'page'  => '\d+',
+                        ));
+
+
+            
+        }
 
 
 }
