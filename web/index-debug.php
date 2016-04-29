246f6ab8574dd30133469770686d6ca254f7d0a3
Zsolt Sándor <ezsolt@gmail.com>
2016-04-29 21:25:27 +0200

    new debug stuff

--- web/index-debug.php
+++ web/index-debug.php
@@ -9,18 +9,29 @@ require dirname(__DIR__) . '/application/Config/AuraViews.php';
 $views = new \Application\Config\AuraViews;
 // $zzz = $views->getViews();
 
+$template_path = realpath( __DIR__ . '/../application/templates');
+$staticpages_path = $template_path . '/staticpages';
+$partials_path = $template_path . '/partials';
+
 $zzz = [
     'views' => [
-        'path' => realpath( __DIR__ . '/../application/auraview'),
+
+        // paths
+        'path' => $template_path,
+        'staticpages_path' => $staticpages_path,
+        'partials_path' => $partials_path,
+
         'layout' => '/layout.php',
-        'error' => '/_error.php',
+
         'partials' => [
             'content' => '/_content.php',
-            'header' => '/_header.php',
-            'footer' => '/_footer.php',
-            ]
         ]
-    ];
+    ]
+];
+
+
+
+
 
 
 
@@ -31,14 +42,46 @@ print_r($zzz);
 // var_dump($zzz);
 echo '<hr/>';
 
-print_r($zzz['views']);
-// var_dump($zzz['views']);
+
+
+// template root path
+$template_path = $zzz['views']['path'];
+
+// layout view path
+$layout = $template_path . $zzz['views']['layout'];
+
+// staticpages view path
+$staticpages =  $zzz['views']['staticpages_path'];
+// print_r( $this->staticpages );
+
+// partials path
+$partials_path = $zzz['views']['partials_path'];
+
+
 echo '<hr/>';
 
-print_r($zzz['views']['path']);
+print_r($template_path);
 
 echo '<hr/>';
 
+
+echo '<hr/>';
+
+print_r($layout);
+
+echo '<hr/>';
+
+echo '<hr/>';
+
+print_r($staticpages);
+
+echo '<hr/>';
+
+echo '<hr/>';
+
+print_r($partials_path);
+
+echo '<hr/>';
 
 
 
