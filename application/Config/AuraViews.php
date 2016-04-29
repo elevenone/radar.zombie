a42a2676d041cf0eb69bf53118eb235569f850ea
Zsolt Sándor <ezsolt@gmail.com>
2016-04-29 21:23:51 +0200

    template view array changes

--- application/Config/AuraViews.php
+++ application/Config/AuraViews.php
@@ -4,36 +4,44 @@ namespace Application\Config;
 
 class AuraViews
 {
-	//
-	public $views;
-		
-	function __construct()
-	{
-		$views = [
-		    'views' => [
-		        'path' => realpath( __DIR__ . '/../auraview'),
-		        'layout' => '/layout.php',
-		        'error' => '/_error.php',
-		        'partials' => [
-		            'content' => '/partials/_content.php',
-		            // 'header' => '/partials/_header.php',
-		            // 'footer' => '/partials/_footer.php',
-		            ]
-		        ]
-		    ];
+    //
+    public $views;
 
-		$this->views = $views;
-	}
+    function __construct()
+    {
 
-	//
+        //
+        $template_path = realpath( __DIR__ . '/../templates');
+        $staticpages_path = $template_path . '/staticpages';
+        $partials_path = $template_path . '/partials';
+
+        $views = [
+            'views' => [
+
+                // paths
+                'path' => $template_path,
+                'staticpages_path' => $staticpages_path,
+                'partials_path' => $partials_path,
+
+                'layout' => '/layout.php',
+
+                'partials' => [
+                    'content' => '/_content.php',
+                ]
+            ]
+        ];
+
+        $this->views = $views;
+    }
+
+    //
     // public function __invoke(){}
 
-	//
-	public function getViews()
-	{
-		return $this->views;
-		
-	}
+    //
+    public function getViews()
+    {
+        return $this->views;
+    }
 
 }
 
