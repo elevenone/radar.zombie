02554012489ce35f586e26c29fd6a666b87f3799
Zsolt Sándor <ezsolt@gmail.com>
2016-05-01 13:23:42 +0200

    update navigation links

--- application/templates/partials/_navigation.php
+++ application/templates/partials/_navigation.php
@@ -2,7 +2,8 @@ navigation
 <hr/>
 
 <a href="/">home</a>
-<a href="/page">page home</a>
+<a href="/page">page </a>
+<a href="/page/">page /</a>
 <a href="/page/mikka">mikka</a>
 <a href="/page/mikka2">mikka 2</a>
 <a href="/page/mikka3">mikka 3</a>
